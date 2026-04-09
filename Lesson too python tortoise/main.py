2b6524743a2d0d57d396a323d92816975188ae65
HugoGosain <hugogosain@gmail.com>
2026-04-09 18:57:27 +0200

    Add files via upload

---
+++ Lesson too python tortoise/main.py
@@ -0,0 +1,30 @@
+import turtle
+
+pen = turtle.Turtle()
+screen = turtle.Screen()
+pen.speed(100)
+
+
+# pen.forward(100)
+# pen.left(90)
+# pen.forward(200)
+# pen.right(90)
+# pen.forward(100)
+# pen.right(90)
+# pen.forward(100)
+# pen.right(90)
+# pen.forward(200)
+# pen.left(90)
+# pen.forward(100)
+# pen.left(90)
+# pen.forward(100)
+
+for i in range(12):
+    pen.forward(105)
+    pen.left(30)
+
+
+# pen.circle(60)
+
+
+turtle.done()
