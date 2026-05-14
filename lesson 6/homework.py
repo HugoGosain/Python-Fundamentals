9c19d4ff8c2bec48aa983b39fb3fb93d5f10a72c
HugoGosain <hugogosain@gmail.com>
2026-05-14 18:14:02 +0200

    Add files via upload

---
+++ lesson 6/homework.py
@@ -0,0 +1,25 @@
+# Theme park ride check:
+
+print("Hello! wlecome to The Pendulum! \n")
+age = int(input("What is your age? "))
+weight = int(input("and what is your wieght? "))
+
+if age > 12 or weight > 35:
+    print("You are allowed here!")
+else:
+    print("You cannot come here yet!")
+
+# Ice cream pack selector:
+
+# ppl = int(input("How many people need ice cream? "))
+
+# if ppl == 1:
+#     print("You should buy the small or regular pack")
+# elif ppl == 2:
+#     print("You should buy the double pack")
+# elif ppl == 3 or ppl == 4:
+#     print("You should buy the family pack")
+# elif ppl == 5 or ppl == 6:
+#     print("You should but the happiness pack")
+# else:
+#     print("You can choose the one you like most")
