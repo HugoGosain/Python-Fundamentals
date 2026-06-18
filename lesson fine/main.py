c83aeb27c6af2750a9ed499f2b3251df59134bed
HugoGosain <hugogosain@gmail.com>
2026-06-18 18:17:04 +0200

    Add files via upload

--- lesson fine/main.py
+++ lesson fine/main.py
@@ -8,13 +8,27 @@ import random
 # print(random.randrange(1,11))
 
 
+# while True:
+#     number = random.randint(0,10)
+#     guess = int(input("what is your guess? "))
+#     if number == guess:
+#         print("YYYYAAAAAAYYY your got it right it was "+str(number))
+#         break
+#     elif number > guess:
+#         print("your number was too low :(")
+#     else:
+#         print("your number was too high :(") 
+
+
 while True:
-    number = random.randint(0,10)
+    number1 = random.randint(0,10)
+    number2 = random.randint(0,10)
+    total = number1*number2
     guess = int(input("what is your guess? "))
-    if number == guess:
-        print("YYYYAAAAAAYYY your got it right it was "+str(number))
+    if total == guess:
+        print("YYYYAAAAAAYYY your got it right it was "+str(total))
         break
-    elif number > guess:
+    elif total > guess:
         print("your number was too low :(")
     else:
         print("your number was too high :(") 
