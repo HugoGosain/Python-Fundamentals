f80bf4ccc5f4e8ee02cfdd71b9f150ac57e8f14e
HugoGosain <hugogosain@gmail.com>
2026-07-02 18:57:33 +0200

    Add files via upload

---
+++ list.py/activity.py
@@ -0,0 +1,5 @@
+list1 = ["GD","Battle cats","BTD6","Roblox","Minecraft"]
+print (list1)
+game = input("Say a game ").lower()
+if game in list1:
+    print("item present")
---
+++ list.py/mian.py
@@ -0,0 +1,35 @@
+list1 = ["red","blue",75,"open blue"]
+print (list1)
+
+#total number of items
+print (len(list1))
+
+
+# accessing items
+print (list1[2])
+
+#negative indexing
+print(list1[-3])
+
+
+# adding itmes
+list1.append("pink")
+print(list1)
+
+# removing itmes
+# list1.remove("blue")
+# print (list1)
+list1.pop(2)
+print (list1)
+
+# changing items
+list1[2]="green"
+print(list1)
+
+# slicing
+slice1 = list1[0:2]
+print (slice1)
+
+list2 = [9,5,7,2]
+list2.sort()
+print (list2)
